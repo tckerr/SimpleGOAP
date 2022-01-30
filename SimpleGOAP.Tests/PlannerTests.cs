a832f080d6ebdb21d714406686d16b44e284bd46
Tom Kerr <tckerr@gmail.com>
2022-01-30 06:14:16 -0500

    River crossing example

--- SimpleGOAP.Tests/PlannerTests.cs
+++ SimpleGOAP.Tests/PlannerTests.cs
@@ -2,6 +2,7 @@ using System;
 using SimpleGOAP.KeyValueState;
 using SimpleGOAP.Tests.Data.ReadmeExample;
 using SimpleGOAP.Tests.Data.Traveler;
+using SimpleGOAP.Tests.Data.Wolf;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -76,6 +77,39 @@ namespace SimpleGOAP.Tests
                 testOutputHelper.WriteLine($"\t{step.Action.Title}");
 
             Assert.True(plan.Success);
+        }
+
+        [Fact]
+        public void TestRiverCrossing()
+        {
+            var data = RiverCrossingDataFactory.Create();
+            var subject = new Planner<RiverCrossingState>(
+                new LambdaCopier<RiverCrossingState>(state => new RiverCrossingState
+                {
+                    Cabbage = state.Cabbage,
+                    Goat = state.Goat,
+                    Wolf = state.Wolf,
+                    Farmer = state.Farmer
+                }),
+                new RiverCrossingStateComparer()
+            );
+
+            var start = DateTime.Now;
+            var plan = subject.Execute(data);
+            var duration = DateTime.Now - start;
+
+            testOutputHelper.WriteLine($"Plan complete after {duration.TotalMilliseconds}ms:");
+            foreach (var step in plan.Steps)
+                testOutputHelper.WriteLine($"\t{step.Action.Title}");
+
+            Assert.True(plan.Success);
+            Assert.Equal("Move goat left", plan.Steps[0].Action.Title);
+            Assert.Equal("Return", plan.Steps[1].Action.Title);
+            Assert.Equal("Move wolf left", plan.Steps[2].Action.Title);
+            Assert.Equal("Move goat right", plan.Steps[3].Action.Title);
+            Assert.Equal("Move cabbage left", plan.Steps[4].Action.Title);
+            Assert.Equal("Return", plan.Steps[5].Action.Title);
+            Assert.Equal("Move goat left", plan.Steps[6].Action.Title);
         }
 
         [Fact]
