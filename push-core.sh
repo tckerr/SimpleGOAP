eaf7798eb3993169ab6d2fdb979baab7eb8c0686
Tom Kerr <tckerr@gmail.com>
2022-02-21 13:20:57 -0500

    Update push-core.sh

--- push-core.sh
+++ push-core.sh
@@ -1,5 +1,21 @@
+if [ -z "$1" ]; then
+    echo "Error: no version number supplied"
+    exit 1
+fi
+
 version=$1
+package_path="./SimpleGOAP.Core/bin/Release/SimpleGOAP.Core.$version.nupkg"
+if [ ! -f $package_path ]; then
+    echo "Error: no .nupkg file for that version exists"
+    exit 1
+fi
+
+if [ ! -f .api_key ]; then
+    echo "Error: no .api_key file exists"
+    exit 1
+fi
+
 api_key=`cat .api_key`
 api_url='https://api.nuget.org/v3/index.json'
 
-nuget push -ApiKey $api_key -Source $api_url ./SimpleGOAP.Core/bin/Release/SimpleGOAP.Core.$version.nupkg
+nuget push -ApiKey $api_key -Source $api_url $package_path
